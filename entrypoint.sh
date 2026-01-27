cdb78e0ce041c334c694cf025a35d5d1975c4cb5
Bruno Paiva <brunobspaiva@gmail.com>
2026-01-27 12:39:57 -0300

    feat: de inicialização script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cat > /usr/share/nginx/html/config.js << EOF
 window.ENV = {
-    OPENAI_API_KEY: "${OPENAI_API_KEY}",
-    OPENAI_MODEL: "${OPENAI_MODEL}",
-    API_ENDPOINT: "${API_ENDPOINT}",
-    CLIENTE_ID: "${CLIENTE_ID}"
+    OPENAI_API_KEY: "${VITE_OPENAI_API_KEY}",
+    OPENAI_MODEL: "${VITE_OPENAI_MODEL}",
+    API_ENDPOINT: "${VITE_API_ENDPOINT}",
+    CLIENTE_ID: "${VITE_CLIENTE_ID}"
 };
 EOF
 exec nginx -g "daemon off;"
